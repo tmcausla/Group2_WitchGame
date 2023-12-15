7d102aeb160bdfa41b94a2727c2b8b359b224a8b
horsetrich <matthewkool12@gmail.com>
2023-12-15 02:25:05 -0500

    Finished orb

--- Assets/FinishGame.cs
+++ Assets/FinishGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishGame : MonoBehaviour
 {
@@ -14,5 +15,12 @@ public class FinishGame : MonoBehaviour
     void Update()
     {
         
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(5);
+        }
     }
 }
